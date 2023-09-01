0f958ca75db784620df2ea5c49975b354972494c
Kamil <kamil@osoms-MacBook-Air.local>
2023-09-01 12:55:30 +0200

    Page rod update

---
+++ web/app/db.php
@@ -0,0 +1,1 @@
+/Users/kamil/Sites/dzialkowik/web/app/plugins/query-monitor/wp-content/db.php
--- web/app/themes/juniper-theme/pages/page-rod.php
+++ web/app/themes/juniper-theme/pages/page-rod.php
@@ -3,14 +3,13 @@
 	Template name: ROD
 */
 
-$funds_array = array();
 
 $context          = Timber::context();
 $args             = array(
 	'post_type'      => 'rod',
 	'posts_per_page' => -1,
 );
-$context['post']  = new Timber\Post();
-$context['posts'] = Timber::get_posts( $args );
+$context['current_post']  = new Timber\Post();
+$context['all_rods'] = Timber::get_posts( $args );
 
 Timber::render( array( 'pages/page-rod.twig' ), $context );
